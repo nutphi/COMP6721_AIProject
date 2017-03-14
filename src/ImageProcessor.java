e9f0bb56bba1bdd5b33ce38d309baaac91628ec3
Nuttakit <nutphi@outlook.com>
2017-03-14 14:36:38 -0400

    Renamed Preprocess to Preprocessor and Fixed normalize bugs

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -9,46 +9,49 @@ import javax.imageio.ImageIO;
 public class ImageProcessor {
 
 	/**
-	 * topview feature
-	 * @param img
-	 * @return
+	 * Top view feature
+	 * @param img BufferedImage that you want to get the feature
+	 * @return Top view array of integer of the image
 	 */
 	public static ArrayList<Integer> getTopViewList(BufferedImage img)
 	{
-		ArrayList<Integer> topViewList = new ArrayList<Integer>();
-		for(int i = 0; i<img.getWidth()-2;++i)
+		ArrayList<Integer> topViewList = new ArrayList<>();
+		for(int i = 0; i<img.getWidth();++i)
 		{
-			for(int j = 0; j<img.getHeight()-1;++j)
+			boolean isWhite=false;
+			for(int j = 0; j<img.getHeight();++j)
 			{
 				try{
 				Color c = new Color(img.getRGB(j, i));
 				if(c.equals(Color.WHITE))
 				{
 					topViewList.add(j);
+					isWhite=true;
 					break;
 				}
 				}catch(Exception e)
 				{
-					System.out.println();
+					System.out.println("i:"+i+",j:"+j);
 				}
 			}
-			
+			if(!isWhite)
+				topViewList.add(img.getHeight());
 		}
 		return topViewList;
 	}
 	
 	/**
-	 * projection feature
-	 * @param img
-	 * @return
+	 * Projection feature
+	 * @param img BufferedImage that you want to get the feature
+	 * @return Array of projection profile of the image
 	 */
 	public static ArrayList<Integer> getProjectionList(BufferedImage img)
 	{
-		ArrayList<Integer> projectionList = new ArrayList<Integer>();
-		for(int i = 0; i<img.getWidth()-2;++i)
+		ArrayList<Integer> projectionList = new ArrayList<>();
+		for(int i = 0; i<img.getWidth();++i)
 		{
 			int whiteNum = 0;
-			for(int j = 0; j<img.getHeight()-1;++j)
+			for(int j = 0; j<img.getHeight();++j)
 			{
 				Color c = new Color(img.getRGB(j, i));
 				if(c.equals(Color.WHITE))
@@ -62,46 +65,57 @@ public class ImageProcessor {
 	}
 	
 	/**
-	 * create empty image 
-	 * @param width
-	 * @param height
-	 * @return
+	 * Create empty image
+	 * @param width Width of the empty image
+	 * @param height Height of the empty image
+	 * @return Empty image
 	 */
-	
 	public static BufferedImage createBufferedImage(int width, int height)
 	{
 		return new BufferedImage(width,height, BufferedImage.TYPE_BYTE_GRAY);
 	}
-	
+
+	/**
+	 * Copy BufferedImage Object
+	 * @param img BufferedImage that you want to copy
+	 * @return Copied BufferedImage Object
+	 */
 	public static BufferedImage copyBufferedImageWithBorder(BufferedImage img)
 	{
 		BufferedImage newImg = new BufferedImage(img.getWidth()+2,img.getHeight()+2, BufferedImage.TYPE_BYTE_GRAY);
-		for (int i = 0; i < img.getWidth(); i++) {
-			for (int j = 0; j < img.getHeight(); j++) {
+		for (int i = 1; i < img.getWidth(); i++) {
+			for (int j = 1; j < img.getHeight(); j++) {
 				try{
 					newImg.setRGB(i+1, j+1, img.getRGB(i, j));
-				}catch(Exception e)
-				{
-					System.out.println("...");
+				}catch(Exception e) {
+					e.printStackTrace();
 				}
-				
 			}
 		}
+		for (int i=0; i < newImg.getWidth();i++){
+
+			newImg.setRGB(i, 0, Color.BLACK.getRGB());
+			newImg.setRGB(i, newImg.getHeight()-1, Color.BLACK.getRGB());
+		}
+		for (int i=0; i < newImg.getHeight();i++){
+			newImg.setRGB(0, i, Color.BLACK.getRGB());
+			newImg.setRGB(newImg.getWidth()-1, i, Color.BLACK.getRGB());
+		}
+
 		return newImg;
 	}
 	
 	/**
 	 * 
-	 * @param inputfile
-	 * @return
+	 * @param inputFile File that you want to get image
+	 * @return Image from the input image file
 	 */
-	public static BufferedImage load(File inputfile)
+	public static BufferedImage load(File inputFile)
 	{
 		BufferedImage img = null;
 		try {
-		    img = ImageIO.read(inputfile);
+		    img = ImageIO.read(inputFile);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return img;
@@ -109,16 +123,15 @@ public class ImageProcessor {
 	
 	/**
 	 * 
-	 * @param img
-	 * @param outputfile
-	 * @return
+	 * @param img Image that you want to save
+	 * @param outputFile Image
+	 * @return true if the file is saved; otherwise false
 	 */
-	public static boolean save(BufferedImage img ,File outputfile)
+	public static boolean save(BufferedImage img ,File outputFile)
 	{
 		try {
-			ImageIO.write(img, "jpg", outputfile);
+			ImageIO.write(img, "jpg", outputFile);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}
---
+++ src/Preprocessor.java
@@ -0,0 +1,93 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+
+public class Preprocessor {
+	
+	public void binarize(BufferedImage img)
+	{	
+		for (int i = 0; i < img.getHeight(); i++) {
+			for (int j = 0; j < img.getWidth(); j++) {
+				Color c = new Color(img.getRGB(j, i));
+				int r = c.getRed();
+				int g = c.getGreen();
+				int b = c.getBlue();
+				int grayScale = (r+g+b)/3;
+				if(grayScale>=240)
+					img.setRGB(j, i, Color.BLACK.getRGB());
+				else
+					img.setRGB(j, i, Color.WHITE.getRGB());
+			}
+		}
+	}
+	
+	public void smooth(BufferedImage img) {
+		//thinning
+		for (int k = 0; k < 1; ++k) {
+			for (int i = 1; i < img.getHeight() - 1; ++i) {
+				for (int j = 1; j < img.getWidth() - 1; ++j) {
+					boolean a = new Color(img.getRGB(j - 1, i - 1)).getRed() != 0;
+					boolean b = new Color(img.getRGB(j - 1, i)).getRed() != 0;
+					boolean c = new Color(img.getRGB(j - 1, i + 1)).getRed() != 0;
+					boolean d = new Color(img.getRGB(j, i - 1)).getRed() != 0;
+					boolean e = new Color(img.getRGB(j, i + 1)).getRed() != 0;
+					boolean f = new Color(img.getRGB(j + 1, i - 1)).getRed() != 0;
+					boolean g = new Color(img.getRGB(j + 1, i)).getRed() != 0;
+					boolean h = new Color(img.getRGB(j + 1, i + 1)).getRed() != 0;
+					if (!(((a || b || d) && (e || g || h)) || ((b || c || e) && (d || f || g)))) {
+						img.setRGB(j, i, Color.BLACK.getRGB());
+					}
+				}
+			}
+		}
+	}
+
+	public BufferedImage normalize(BufferedImage img)
+	{
+		BufferedImage imgBorder= ImageProcessor.copyBufferedImageWithBorder(img);
+
+		int afterWidth = 564;
+		int afterHeight = 564;
+		double w_alpha1 = afterWidth / (imgBorder.getWidth()-2);
+		double h_beta1 = afterHeight / (imgBorder.getHeight()-2);
+
+		BufferedImage normalizedImg = ImageProcessor.createBufferedImage(afterWidth, afterHeight);
+		int old_width = 0;
+		int old_height = 0;
+		for (int i = 1; i <= afterHeight; ++i)
+		{
+			for (int j = 1; j <= afterWidth; ++j)
+			{
+				old_width = (int) Math.floor(j/w_alpha1);
+				old_width = old_width > imgBorder.getWidth()-1?imgBorder.getWidth()-1:old_width;
+				old_height = (int) Math.floor(i/ h_beta1);
+				old_height = old_height > imgBorder.getHeight()-1?imgBorder.getHeight()-1:old_height;
+
+				int rgb = 0;
+				try{
+					rgb = imgBorder.getRGB(old_width,old_height);
+				}catch(Exception e)
+				{
+					System.out.println("...");
+				}
+				normalizedImg.setRGB(j-1, i-1, rgb);
+			}
+		}
+		return normalizedImg;
+	}
+	
+	public static void main(String[] args) {
+		Preprocessor p = new Preprocessor();
+		BufferedImage b = ImageProcessor.load(new File("0/ID00002_p1_B12_GL.png"));
+		p.binarize(b);
+		p.smooth(b);
+		ImageProcessor.save(p.normalize(b), new File("image1.jpg"));
+		ArrayList<Integer> topView = ImageProcessor.getTopViewList(p.normalize(b));
+		System.out.println(topView.size());
+		for(int n:topView)
+		{
+			System.out.println(":"+n);
+		}
+	}
+}
